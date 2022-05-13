9fd9af520ab6be4a9e78bd50b30240b7e3fdcec8
Arekuso-kun <alexodrino@gmail.com>
2022-05-13 03:09:23 +0300

    commit

--- Assets/Buttons.cs
+++ Assets/Buttons.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Buttons : MonoBehaviour
 {
@@ -14,17 +16,34 @@ public class Buttons : MonoBehaviour
     public Rigidbody2D m_Rigidbody2D;
     public Player_Movement m_player_Movement;
 
+    public Volume m_Volume;
+    DepthOfField depthOfFieldValue;
+
     void Start()
     {
         RestartScreen.SetActive(false);
+        DepthOfField dof;
+        if(m_Volume.profile.TryGet<DepthOfField>(out dof)) { depthOfFieldValue = dof; }
+        depthOfFieldValue.active = true;
         Time.timeScale = 0;
     }
 
     void Update()
     {
+        Debug.Log("m_collision.Dead = " + m_collision.Dead.ToString());
+        Debug.Log("\ndepthOfFieldValue.active = " + depthOfFieldValue.active.ToString());
+        Debug.Log("\nTime.timeScale = " + Time.timeScale.ToString());
+
         if (m_collision.Dead == true)
         {
             RestartScreen.SetActive(true);
+            depthOfFieldValue.active = true;
+        }
+        else
+        {
+            if (Time.timeScale != 0)
+                depthOfFieldValue.active = false;
+            else depthOfFieldValue.active = true;
         }
     }
 
